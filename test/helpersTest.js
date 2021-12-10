27430f3398342d1ee46d37d78fb9247d5f8a9b36
Stefan Darrell Davis <stefan.davis@mail.utoronto.ca>
2021-12-10 00:47:26 +0000

    Added More Tests

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,8 +1,6 @@
-const {
-  assert
-} = require('chai');
+const { assert } = require('chai');
 
-const randomString = require('../helpers');
+const { randomString, emailHasUser, userIdFromEmail, urlsForUser, cookieHasUser } = require('../helpers');
 
 describe('randomString', function() {
 
@@ -16,5 +14,74 @@ describe('randomString', function() {
       const firstRandomString = randomString();
       const secondRandomString = randomString();
       assert.notEqual(firstRandomString, secondRandomString);
+  });
+});
+
+describe('userIdFromEmail', function() {
+
+  it('should return a user with a valid email', function() {
+    const user = userIdFromEmail("user1@example.com", testUsers);
+    const expectedOutput = "user1RandomID";
+    assert.equal(user, expectedOutput);
+  });
+
+  it('should return undefined when no user exists for a given email address', function() {
+    const user = userIdFromEmail("me@test.com", testUsers);
+    const expectedOutput = undefined;
+    assert.equal(user, expectedOutput);
+  });
+});
+
+describe('emailHasUser', function() {
+  
+  it('should return true if email corresponds to a user in the database', function() {
+    const existingEmail = emailHasUser("user1@example.com", testUsers);
+    const expectedOutput = true;
+    assert.equal(existingEmail, expectedOutput);
+  });
+
+  it('should return false if email does not correspond to a user in the database', function() {
+    const nonExistantEmail = emailHasUser("fake_email@test.com", testUsers);
+    const expectedOutput = false;
+    assert.equal(nonExistantEmail, expectedOutput);
+  });
+});
+
+describe('urlsForUser', function() {
+
+  it('should return an object of url information specific to the given user ID', function() {
+    const specificUrls = urlsForUser("user1RandomID", testUrlDatabase);
+    const expectedOutput = {
+      "bfjqot": {
+        longUrl: "http://www.lighthouselabs.ca",
+        userID: "user1RandomID"
+      },
+      "htlams": {
+        longUrl: "http://www.google.com",
+        userID: "user1RandomID"
+      }
+    };
+    assert.deepEqual(specificUrls, expectedOutput);
+  });
+
+  it('should return an empty object if no urls exist for a given user ID', function() {
+    const noSpecificUrls = urlsForUser("fakeUser", testUrlDatabase);
+    const expectedOutput = {};
+    assert.deepEqual(noSpecificUrls, expectedOutput);
+  });
+});
+
+describe('cookieHasUser', function() {
+
+  it('should return true if a cookie corresponds to a user in the database', function() {
+    const existingCookie = cookieHasUser("user1RandomID", testUsers);
+    const expectedOutput = true;
+    assert.equal(existingCookie, expectedOutput);
+  });
+
+  it('should return false if a cookie does not correspond to a user in the database', function() {
+    const nonExistantCookie = cookieHasUser("user3RandomID", testUsers);
+    const expectedOutput = false;
+    assert.equal(nonExistantCookie, expectedOutput);
   });
 });
